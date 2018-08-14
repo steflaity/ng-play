e6636b2edaf3a7f0a4888ed9704e90ec39f7617b
Stefan Laity <stefan.laity@esoterix.co.uk>
2018-08-14 18:11:13 +0100

    lint

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -23,5 +23,5 @@ import { Component } from '@angular/core';
 `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app'
 }
